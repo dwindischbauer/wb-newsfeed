aea19b881b815063966f4536ea2ecc163d1f2ee1
Stefan Schachner <s.schachner1@students.htl-leonding.ac.at>
2026-07-04 17:10:45 +0000

    feat(feed): init nuxt feed app with vertical snap scroll

---
+++ apps/feed/app.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="feed-container">
+    <NuxtPage />
+  </div>
+</template>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #000;
+  color: #fff;
+  font-family: 'Inter', sans-serif;
+}
+.feed-container {
+  height: 100vh;
+  width: 100%;
+  overflow: hidden;
+}
+</style>
