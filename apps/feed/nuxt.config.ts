84d8a0b8874c80256882796541617f3768476dde
Stefan Schachner <s.schachner1@students.htl-leonding.ac.at>
2026-07-11 08:16:42 +0000

    chore(feed): extract api url to config

--- apps/feed/nuxt.config.ts
+++ apps/feed/nuxt.config.ts
@@ -2,5 +2,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   future: {
     compatibilityVersion: 4,
+  },
+  runtimeConfig: {
+    public: {
+      apiUrl: process.env.API_URL || 'http://localhost:3005'
+    }
   }
 });
