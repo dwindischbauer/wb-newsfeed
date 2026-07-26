cb8669ba2aec310afd8de366081c9bcb135d210c
Stefan Schachner <s.schachner1@students.htl-leonding.ac.at>
2026-07-26 19:05:27 +0000

    feat(feed): apply font optimization configuration

--- apps/feed/nuxt.config.ts
+++ apps/feed/nuxt.config.ts
@@ -10,6 +10,11 @@ export default defineNuxtConfig({
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' },
         { name: 'theme-color', content: '#000000' }
+      ],
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;800&display=swap' }
       ]
     }
   },
