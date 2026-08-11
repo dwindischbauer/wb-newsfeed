dd0fdd8bbd416ae2f901e57cdfb82963007ee0cc
David Windischbauer <d.windischbauer@students.htl-leonding.ac.at>
2026-08-11 19:03:46 +0000

    chore(api): fix default db credentials for local dev

--- apps/api/src/db/index.ts
+++ apps/api/src/db/index.ts
@@ -7,7 +7,7 @@ if (!process.env.DATABASE_URL) {
 }
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL || 'postgresql://admin:adminpassword@localhost:5433/wbnews'
+  connectionString: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5433/wbnews'
 });
 
 export const db: NodePgDatabase<typeof schema> = drizzle(pool, { schema });
