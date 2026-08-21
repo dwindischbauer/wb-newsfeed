1defea72f9c4e8ac465cc9c959a066f7ef7ad0fa
Stefan Schachner <s.schachner1@students.htl-leonding.ac.at>
2026-08-21 20:11:12 +0200

    chore(feed): add meta tags for mobile rendering

--- apps/feed/nuxt.config.ts
+++ apps/feed/nuxt.config.ts
@@ -3,6 +3,16 @@ export default defineNuxtConfig({
   future: {
     compatibilityVersion: 4,
   },
+  app: {
+    head: {
+      title: 'WB News Feed',
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' },
+        { name: 'theme-color', content: '#000000' }
+      ]
+    }
+  },
   runtimeConfig: {
     public: {
       apiUrl: process.env.API_URL || 'http://localhost:3005'
